c613a8ffcecd0c225a105e9d82aaf5bc213bc0e5
KimSeal <56949110+KimSeal@users.noreply.github.com>
2021-09-27 23:44:59 +0900

    Update shortcut.h

--- shortcut.h
+++ shortcut.h
@@ -10,23 +10,23 @@ class Node{
 	vector<int>row2;
 	vector<int>col2;
 	vector<int>move;
-	void node_Make(int oldrow, int oldcol, int olddist){
+	void nodeMake(int oldrow, int oldcol, int olddist){
 		this->row=oldrow;
 		this->col=oldcol;
 		this->dist=olddist;
 	}
-	void node_vec(int row1, int col1){
+	void nodeVec(int row1, int col1){
 		row2.push_back(row1);
 		col2.push_back(col1);
 	}
-	void node_del(){
+	void nodeDel(){
 		row2.pop_back();
 		col2.pop_back();
 	}
-	void node_move(int a){
+	void nodeMove(int a){
 		move.push_back(a);
 	}
-	void node_move_del(){
+	void nodeMoveDel(){
 		move.pop_back();
 	}
 };
@@ -55,15 +55,15 @@ void bfs(int n,int m,int meY,int meX,int juY, int juX,int oldmap[7][7],int sight
 	
 	queue<Node> q;
 	Node mainnode, mainnode2;
-	mainnode.node_Make(meY,meX,0);
-	mainnode.node_vec(meY,meX);
-	mainnode.node_move(7);
+	mainnode.nodeMake(meY,meX,0);
+	mainnode.nodeVec(meY,meX);
+	mainnode.nodeMove(7);
 	q.push(mainnode);
 	mainnode.row2.clear();
 	mainnode.col2.clear();
 	mainnode.move.clear();
 	while(!q.empty()){
-		mainnode.node_Make(q.front().row, q.front().col, q.front().dist);
+		mainnode.nodeMake(q.front().row, q.front().col, q.front().dist);
 		mainnode.row2.clear();
 		mainnode.col2.clear();
 		mainnode.move.clear();
@@ -75,8 +75,8 @@ void bfs(int n,int m,int meY,int meX,int juY, int juX,int oldmap[7][7],int sight
         curDist = mainnode.dist;
 		
 		for(int i=0;i<=curDist;i++){//vector<int> 
-			mainnode2.node_vec(q.front().row2[i],q.front().col2[i]); 
-			mainnode2.node_move(q.front().move[i]);
+			mainnode2.nodeVec(q.front().row2[i],q.front().col2[i]); 
+			mainnode2.nodeMove(q.front().move[i]);
 		}
 
 		if(curRow == juY){
@@ -96,39 +96,39 @@ void bfs(int n,int m,int meY,int meX,int juY, int juX,int oldmap[7][7],int sight
        
         // 위로 갈수 있으면
         if(curRow-1 >= 0 && map[curRow-1][curCol] == 0) {
-			mainnode2.node_Make(curRow-1, curCol, curDist+1);
-			mainnode2.node_vec(curRow-1,curCol);
-			mainnode2.node_move(1);
+			mainnode2.nodeMake(curRow-1, curCol, curDist+1);
+			mainnode2.nodeVec(curRow-1,curCol);
+			mainnode2.nodeMove(1);
             q.push(mainnode2);
-			mainnode2.node_del();
-			mainnode2.node_move_del();
+			mainnode2.nodeDel();
+			mainnode2.nodeMoveDel();
         }
         // 아래로 갈수 있으면
         if(curRow+1 < n1 && map[curRow+1][curCol] == 0) {
-            mainnode2.node_Make(curRow+1, curCol, curDist+1);
-			mainnode2.node_vec(curRow+1,curCol);
-			mainnode2.node_move(3);
+            mainnode2.nodeMake(curRow+1, curCol, curDist+1);
+			mainnode2.nodeVec(curRow+1,curCol);
+			mainnode2.nodeMove(3);
 			q.push(mainnode2);
-			mainnode2.node_del();
-			mainnode2.node_move_del();
+			mainnode2.nodeDel();
+			mainnode2.nodeMoveDel();
         }
         // 왼쪽으로 갈수 있으면
         if(curCol-1 >= 0 && map[curRow][curCol-1] == 0) {
-            mainnode2.node_Make(curRow, curCol-1, curDist+1);
-			mainnode2.node_vec(curRow,curCol-1);
-			mainnode2.node_move(4);
+            mainnode2.nodeMake(curRow, curCol-1, curDist+1);
+			mainnode2.nodeVec(curRow,curCol-1);
+			mainnode2.nodeMove(4);
 			q.push(mainnode2);
-			mainnode2.node_del();
-			mainnode2.node_move_del();
+			mainnode2.nodeDel();
+			mainnode2.nodeMoveDel();
         }
     	// 오른쪽으로 갈수 있으면
         if(curCol+1 < m1 && map[curRow][curCol+1] == 0) {
-            mainnode2.node_Make(curRow, curCol+1, curDist+1);
-			mainnode2.node_vec(curRow,curCol+1);
-			mainnode2.node_move(2);
+            mainnode2.nodeMake(curRow, curCol+1, curDist+1);
+			mainnode2.nodeVec(curRow,curCol+1);
+			mainnode2.nodeMove(2);
         	q.push(mainnode2);
-			mainnode2.node_del();
-			mainnode2.node_move_del();
+			mainnode2.nodeDel();
+			mainnode2.nodeMoveDel();
 		}
 	}
 	//만들어진 가로, 세로 배열 복사
