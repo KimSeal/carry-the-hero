1eafee70e9297f4acb53d1514452c380e644047c
KimSeal <56949110+KimSeal@users.noreply.github.com>
2021-10-21 22:48:52 +0900

    Update player.h

--- player.h
+++ player.h
@@ -45,3 +45,44 @@ void playerMove(character * player, int (*newmap)[7]){
 		}
 	}
 }
+
+struct monster{
+	int Y;
+	int X;
+	int movement[30];
+};
+
+void monsterMove(int i, monster * mon1, int (*newmap)[7], int m, int n){
+	if(mon1->movement[i]==1){
+		if((mon1->Y-1)>=0&&newmap[mon1->Y-1][mon1->X]==0){
+			newmap[mon1->Y][mon1->X]=0;
+			mon1->Y-=1;
+			newmap[mon1->Y][mon1->X]=3;
+			cout<<"go up\n";
+		}
+	}
+	else if(mon1->movement[i]==2){
+		if((mon1->X+1)<m && newmap[mon1->Y][mon1->X+1]==0){
+			newmap[mon1->Y][mon1->X]=0;
+			mon1->X+=1;
+			newmap[mon1->Y][mon1->X]=3;
+			cout<<"go right\n";
+		}
+	}
+	else if(mon1->movement[i]==3){
+		if((mon1->Y+1)<n&&newmap[mon1->Y+1][mon1->X]==0){
+			newmap[mon1->Y][mon1->X]=0;
+			mon1->Y+=1;
+			newmap[mon1->Y][mon1->X]=3;
+			cout<<"go down\n";
+		}
+	}
+	else if(mon1->movement[i]==4){
+		if((mon1->X-1)>=0 && newmap[mon1->Y][mon1->X-1]==0){
+			newmap[mon1->Y][mon1->X]=0;
+			mon1->X-=1;
+			newmap[mon1->Y][mon1->X]=3;
+			cout<<"go left\n";
+		}
+	}	
+}	
